a1ca7a969313a0ddc7f5c2fdbaf4fa1ec7d61fc2
Kurizle <68846461+Kurizle@users.noreply.github.com>
2022-07-13 03:14:34 +0200

    Update main.h

--- main.h
+++ main.h
@@ -2,18 +2,14 @@
 #define _main_h_
 
 #include <stdarg.h>
-
 #include <unistd.h>
-
 #include <stdlib.h>
 
 /**
-
-* struct print - struct for printer functions
+* struct print - printer functions
 * @type_arg: identifier
 * @f: pointer to a printer functions
-* Description: struct that stores pointers to a
-* printer functions.
+* Description: struct that stores pointers to a printer functions.
 */
 
 typedef struct print
