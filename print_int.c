a2ccdf67a3529f41e8d77b08df3195b90cff1098
“Kurai <klgombiro@live.com>
2022-07-14 11:01:42 -0700

    Prints numbers in base 10

--- print_int.c
+++ print_int.c
@@ -4,9 +4,11 @@
  * print_integer - Print a number in base 10
  * @list: Number to print in base 10
  *
- * Return: Length of th numbers in decimal
+ * Return: Length of the numbers in decimal
  **/
+
 int print_integer(va_list list)
+
 {
 	char *p_buff;
 	int size;
